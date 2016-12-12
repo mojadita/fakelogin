8bc78924f6445d2584cab9e9315012d360f3e4cb
Luis Colorado <luiscoloradourcola@gmail.com>
2016-12-12 20:15:46 +0200

    Copy BSD copyright notice into login.c header
    Signed-off-by: Luis Colorado <luiscoloradourcola@gmail.com>

--- login.c
+++ login.c
@@ -3,16 +3,42 @@
  * Date: Mon Dec 12 19:59:55 EET 2016
  * Disclaimer: (C) 2016 Luis Colorado.  All rights reserved.
  *
- * This program is open source according to the BSD license.
- * No warranty of use is granted.  You are on your own.
- * You can distribute this program in source or binary form,
- * assumed that you credit the author and respect the copyright
- * note.  This means you are expressely not allowed to change
- * this copyright notice, the author's name above and you 
- * commit to add a text note with this text to any binary
- * distribution you make of this program.  You can use this
- * program, edit, change, but always respecting the present
- * copyright notice.
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2016, Luis Colorado All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or
+ * without modification, are permitted provided that the
+ * following conditions are met:
+ * 
+ * Redistributions of source code must retain the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above
+ * copyright notice, this list of conditions and the following
+ * disclaimer in the documentation and/or other materials
+ * provided with the distribution.
+ * 
+ * Neither the name of the copyright holder nor the names of
+ * its contributors may be used to endorse or promote products
+ * derived from this software without specific prior written
+ * permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ * CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
  */
 #include <stdarg.h>
 #include <stdio.h>
